68ecb3049fd9c1390a82045910244e0e49ade0dd
Aris <ariskak14@gmail.com>
2019-09-02 02:06:35 +0300

    8th

--- src/js/script.js
+++ src/js/script.js
@@ -1,7 +1,7 @@
-    let boy = document.querySelector(".boy");
     let left = document.querySelector(".left");
     let middle = document.querySelector(".middle");
-
+    let occupation = ["ranger", "warrior", "mage"];
+    let clothing = ["leather clothing", "iron armor", "tattered rags"];
 
     function Person(name, occupation, clothing) {
         this.name = name;
@@ -13,18 +13,15 @@
 
     function randomizer() {
         let random = Math.floor(Math.random() * 3);
-        let occupation = ["ranger", "warrior", "mage"];
-        let clothing = ["leather clothing", "iron armor", "tattered rags"];
         let randomPerson = new Person(faker.name.findName(), occupation[random], clothing[random]);
-        let description = [randomPerson.name + " is a " + occupation[random] + ", wearing " + clothing[random]];
+        let description = randomPerson.name + " is a " + occupation[random] + ", wearing " + clothing[random];
         left.textContent = randomPerson.name;
         middle.textContent = description;
-        console.log(randomPerson.name)
         console.log(randomPerson);
     }
 
     randomizer();
 
-    boy.onclick = function () {
+    document.querySelector(".boy").onclick = function () {
         randomizer();
     }
