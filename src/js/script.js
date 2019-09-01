41e4bb19d39e8f471ee4b816a80231ca80041835
Aris <ariskak14@gmail.com>
2019-09-02 00:10:28 +0300

    sixth commit

--- src/js/script.js
+++ src/js/script.js
@@ -1,16 +1,21 @@
-    var randomName = faker.name.findName(); // Caitlyn Kerluke
-    var randomEmail = faker.internet.email(); // Rusty@arne.info
-    var randomCard = faker.helpers.createCard(); // random contact card containing many properties]
-    var boy = document.querySelector(".boy");
-    var left = document.querySelector(".left");
-    var middle = document.querySelector(".middle");
-    var occupation = [" ranger ", " warrior ", " mage "];
-    var clothing = [" rags ", " steel armor ", " snakeskin "];
-    var random = Math.floor(Math.random() * 2);
-    var description = [randomName + " is a " + occupation[random] + ", wearing " + clothing.random];
+    let randomName = faker.name.findName(); // Caitlyn Kerluke
+    let randomCard = faker.helpers.createCard(); // random contact card containing many properties]
+    let boy = document.querySelector(".boy");
+    let left = document.querySelector(".left");
+    let middle = document.querySelector(".middle");
 
-    boy.onclick = function () {
+    function randomizer() {
+        let randomName = faker.name.findName(); // Caitlyn Kerluke
+        let occupation = ["ranger", "warrior", "mage"];
+        let clothing = ["leather clothing", "iron armor", "tattered rags"];
+        let random = Math.floor(Math.random() * 3);
+        let description = [randomName + " is a " + occupation[random] + ", wearing " + clothing[random]];
         left.textContent = randomName;
         middle.textContent = description;
         console.log(randomName);
+    }
+
+    // boy.onclick = randomizer();
+    boy.onclick = function () {
+        randomizer();
     }
