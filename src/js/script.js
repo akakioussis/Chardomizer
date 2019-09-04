2f8fb270cdc639ac7ef313016a4d90df3ad8b5c2
Aris <ariskak14@gmail.com>
2019-09-04 04:10:52 +0300

    complete

--- src/js/script.js
+++ src/js/script.js
@@ -1,77 +1,14 @@
-// /* Objects */
-
-// function Occupation(name) {
-//     this.name = name;
-// }
-
-// function Clothing(name, quality) {
-//     this.name = name;
-//     this.quality = quality;
-// }
-
-// function Person(name) {
-//     this.name = faker.name.findName();
-//     this.occupation = new Occupation("Unemployed");
-
-//     this.setOccupation = function (occupation) {
-//         this.occupation = occupation;
-//     };
-
-//     this.setClothes = function (clothes) {
-//         this.clothes = clothes;
-//     };
-// }
-
-
-// /* Other stuff */
-// let left = document.querySelector(".left");
-// let middle = document.querySelector(".middle");
-
-
-// const occupations = [new Occupation("Warrior"), new Occupation("Mage"), new Occupation("Ranger")];
-// const clothes = [new Clothing("Leather", "Common"), new Clothing("Copper Chestplate", "Uncommon")];
-
-// function getRandom(array) {
-//     return array[Math.floor(Math.random() * array.length)];
-// }
-
-// function generatePerson() {
-//     let person = new Person(faker.name.findName());
-//     person.setOccupation(getRandom(occupations));
-//     person.setClothes(getRandom(clothes));
-//     console.log(person);
-//     return person;
-// }
-
-// function displayPerson(person) {
-//     left.textContent = person.name;
-//     middle.textContent = person.name + " is a " + person.occupation.name;
-// }
-
-// // function displayPerson() {
-// //     left.textContent = person.name;
-// //     middle.textContent = person.name + " is a " + person.occupation.name;
-// // }
-
-// document.querySelector(".boy").onclick = function () {
-//     generatePerson();
-//     displayPerson();
-// }
-
-// // displayPerson();
-
-
-
-
-/* Objects */
-
 function Occupation(name) {
     this.name = name;
 }
 
-function Clothing(name, quality) {
+function Clothing(name) {
     this.name = name;
+}
+
+function Quality(quality, percentage) {
     this.quality = quality;
+    this.percentage = percentage;
 }
 
 function Person() {
@@ -85,11 +22,17 @@ function Person() {
     this.setClothes = function (clothes) {
         this.clothes = clothes;
     };
+
+    this.setQuality = function (quality, percentage) {
+        this.quality = quality;
+        this.percentage = percentage;
+    }
 }
 
 /* Other stuff */
 const occupations = [new Occupation("Warrior"), new Occupation("Mage"), new Occupation("Ranger")];
-const clothes = [new Clothing("Leather", "Common"), new Clothing("Copper Chestplate", "Uncommon")];
+const clothes = [new Clothing("Leather Armor"), new Clothing("Copper Chestplate")];
+const quality = [new Quality("Common", 10), new Quality("Masterwork", 30), new Quality("Legendary", 100)];
 
 function getRandom(array) {
     return array[Math.floor(Math.random() * array.length)];
@@ -100,13 +43,14 @@ function generatePerson() {
 
     person.setOccupation(getRandom(occupations));
     person.setClothes(getRandom(clothes));
+    person.setQuality(getRandom(quality));
 
     return person;
 }
 
 function displayPerson(person) {
-    document.querySelector(".left").textContent = person.name;
-    document.querySelector(".middle").textContent = person.name + " is a " + person.occupation.name;
+    document.querySelector(".charname").textContent = person.name;
+    document.querySelector(".chardesc").textContent = person.name + " is a " + person.occupation.name + ". " + "He's wearing a " + person.quality.quality + " " + person.clothes.name + ". It offers " + person.quality.percentage + " points of defense.";
 }
 
 document.querySelector(".boy").onclick = function () {
