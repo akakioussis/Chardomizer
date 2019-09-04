feca1e227202fcfea678140deb755a270aa92f9f
Aris <ariskak14@gmail.com>
2019-09-04 03:21:39 +0300

    thank you my g

--- src/js/script.js
+++ src/js/script.js
@@ -1,33 +1,115 @@
-    let left = document.querySelector(".left");
-    let middle = document.querySelector(".middle");
-    let occupation = ["ranger", "warrior", "mage"];
-    let clothing = [new Clothing("Leather Armor", "simple"), new Clothing("Iron Armor", "masterwork"), new Clothing("Hide Rags", "shitty")];
+// /* Objects */
 
-    function Person(name, occupation) {
-        this.name = name;
+// function Occupation(name) {
+//     this.name = name;
+// }
+
+// function Clothing(name, quality) {
+//     this.name = name;
+//     this.quality = quality;
+// }
+
+// function Person(name) {
+//     this.name = faker.name.findName();
+//     this.occupation = new Occupation("Unemployed");
+
+//     this.setOccupation = function (occupation) {
+//         this.occupation = occupation;
+//     };
+
+//     this.setClothes = function (clothes) {
+//         this.clothes = clothes;
+//     };
+// }
+
+
+// /* Other stuff */
+// let left = document.querySelector(".left");
+// let middle = document.querySelector(".middle");
+
+
+// const occupations = [new Occupation("Warrior"), new Occupation("Mage"), new Occupation("Ranger")];
+// const clothes = [new Clothing("Leather", "Common"), new Clothing("Copper Chestplate", "Uncommon")];
+
+// function getRandom(array) {
+//     return array[Math.floor(Math.random() * array.length)];
+// }
+
+// function generatePerson() {
+//     let person = new Person(faker.name.findName());
+//     person.setOccupation(getRandom(occupations));
+//     person.setClothes(getRandom(clothes));
+//     console.log(person);
+//     return person;
+// }
+
+// function displayPerson(person) {
+//     left.textContent = person.name;
+//     middle.textContent = person.name + " is a " + person.occupation.name;
+// }
+
+// // function displayPerson() {
+// //     left.textContent = person.name;
+// //     middle.textContent = person.name + " is a " + person.occupation.name;
+// // }
+
+// document.querySelector(".boy").onclick = function () {
+//     generatePerson();
+//     displayPerson();
+// }
+
+// // displayPerson();
+
+
+
+
+/* Objects */
+
+function Occupation(name) {
+    this.name = name;
+}
+
+function Clothing(name, quality) {
+    this.name = name;
+    this.quality = quality;
+}
+
+function Person() {
+    this.name = faker.name.findName();
+    this.occupation = new Occupation("Unemployed");
+
+    this.setOccupation = function (occupation) {
         this.occupation = occupation;
-    }
+    };
 
-    function Clothing(material, quality) {
-        this.material = material;
-        this.quality = quality;
-    }
+    this.setClothes = function (clothes) {
+        this.clothes = clothes;
+    };
+}
 
-    function randomizer() {
-        let random = Math.floor(Math.random() * 3);
-        let randomPerson = new Person(faker.name.findName(), occupation[random]);
-        let randomClothing = new Clothing(clothing[random].quality, clothing[random].material);
-        let description = randomPerson.name + " is a " + occupation[random] + ", wearing a " + clothing[random].quality + " " + clothing[random].material;
+/* Other stuff */
+const occupations = [new Occupation("Warrior"), new Occupation("Mage"), new Occupation("Ranger")];
+const clothes = [new Clothing("Leather", "Common"), new Clothing("Copper Chestplate", "Uncommon")];
 
-        left.textContent = randomPerson.name;
-        middle.textContent = description;
+function getRandom(array) {
+    return array[Math.floor(Math.random() * array.length)];
+}
 
-        console.log(randomPerson);
-        console.log(randomClothing);
-    }
+function generatePerson() {
+    let person = new Person();
 
-    // randomizer();
+    person.setOccupation(getRandom(occupations));
+    person.setClothes(getRandom(clothes));
 
-    document.querySelector(".boy").onclick = function () {
-        randomizer();
-    }
+    return person;
+}
+
+function displayPerson(person) {
+    document.querySelector(".left").textContent = person.name;
+    document.querySelector(".middle").textContent = person.name + " is a " + person.occupation.name;
+}
+
+document.querySelector(".boy").onclick = function () {
+    let generatedPerson = generatePerson();
+    displayPerson(generatedPerson);
+};
